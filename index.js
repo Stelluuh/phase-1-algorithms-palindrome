9eb0d697bb99dacab1caaf0953af5374e2ce0e54
stellamunson <sftamesis13@gmail.com>
2022-07-14 15:39:27 -0700

    commit tree c3c6a729eb79ff0a6c89bec509e0017898b9a95d

--- index.js
+++ index.js
@@ -1,9 +1,44 @@
+function reverse(word) {
+// //separate the individual characters into an array of characters
+//   const sepChar = word.split('')
+// //reverse the order of the array
+//   const reversedArray = sepChar.reverse()
+// //join the array of characters back together so that it is a single word
+//   const reversedWord = reversedArray.join('')
+// //return the reversed word
+//   return reversedWord
+
+  // or you can write the above code like this:
+  return word.split('').reverse().join('');
+}
+
+
 function isPalindrome(word) {
   // Write your algorithm here
+  const reversedWord = reverse(word)
+  if(word === reversedWord) {
+    //return true
+    return true
+  } else {
+    return false
+  }
 }
 
 /* 
   Add your pseudocode here
+  
+  convert string into individual character
+    => expect 'mom' to equal 'm', 'o', 'm'
+  
+  grab individual character and return it in reverse
+    => expect 'b', 'o', 'y' to equal 'y', 'o', 'b'
+  
+  put individual characters together
+    => expect 'y', 'o', 'b' to equal 'yob'
+  
+  if string is a Palindrome, then return true
+  
+  if string is not a Palindrome, then return false
 */
 
 /*
